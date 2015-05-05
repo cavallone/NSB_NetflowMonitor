626b74deb38cff3c8155d02c5753c2bb6754021c
cavallone <cavallone@hotmail.com.tw>
2015-05-05 21:13:02 +0800

    write flow of 10.0.0.0/8 in another table

--- parse.py
+++ parse.py
@@ -29,8 +29,12 @@ for f in flows:
 	sport = item[4].split(':')[1]
 	dip = item[6].split(':')[0]
 	dport = item[6].split(':')[1]
-	sql = 'INSERT INTO record(date, time, protocol, src_ip, src_port, dst_ip, dst_port)VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\')'.format(item[0], starttime[:8], item[3], sip, sport, dip, dport)
-
+	
+	if sip.split('.')[0] == '10' and dip.split('.')[0] == '10' and dip.split('.')[3] != '255' :
+		sql = 'INSERT INTO record2(date, time, protocol, src_ip, src_port, dst_ip, dst_port)VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\')'.format(item[0], starttime[:8], item[3], sip, sport, dip, dport)
+	else:
+		sql = 'INSERT INTO record(date, time, protocol, src_ip, src_port, dst_ip, dst_port)VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\')'.format(item[0], starttime[:8], item[3], sip, sport, dip, dport)
+	
 	try:
 		cursor.execute(sql)
 		db.commit()
