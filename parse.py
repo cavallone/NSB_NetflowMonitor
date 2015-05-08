e2e351efabfb96a2f9efd2f2114a5f0df166e294
cavallone <cavallone@hotmail.com.tw>
2015-05-08 15:23:57 +0800

    add host name

--- parse.py
+++ parse.py
@@ -7,6 +7,9 @@ import datetime
 import MySQLdb
 from datetime import datetime, timedelta
 
+namedict = {'10.1.1.2':'(Staff)', '10.1.3.2':'(WebServer)', '10.1.4.2':'(DBServer)', '10.1.5.3':'(BossComputer)', '10.1.2.2':'(MailServer)',
+           '10.1.5.2':'(NAT)', '10.1.2.3':'(NAT)', '10.1.3.3':'(NAT)', '10.1.4.3':'(NAT)', '10.1.1.3':'(NAT)'}
+
 now = datetime.now() - timedelta(seconds=90)
 nowdate = datetime.strftime(now, '%Y-%m-%d')
 nowtime = datetime.strftime(now, '%Y%m%d%H%M')
@@ -29,6 +32,16 @@ for f in flows:
 	sport = item[4].split(':')[1]
 	dip = item[6].split(':')[0]
 	dport = item[6].split(':')[1]
+
+	if sip in namedict:
+		sip = sip + namedict[sip]
+	else:
+		pass
+
+	if dip in namedict:
+		dip = dip + namedict[dip]
+	else:
+		pass
 	
 	if sip.split('.')[0] == '10' and dip.split('.')[0] == '10' and dip.split('.')[3] != '255' :
 		sql = 'INSERT INTO record2(date, time, protocol, src_ip, src_port, dst_ip, dst_port)VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\')'.format(item[0], starttime[:8], item[3], sip, sport, dip, dport)
